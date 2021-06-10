ceec7781380b65103db0d4f7b900affc858ff803
Ben Ballard <coolninjagames@gmail.com>
2021-06-10 01:04:23 -0400

    Update Direction.java

--- src/aquaflow/Direction.java
+++ src/aquaflow/Direction.java
@@ -6,4 +6,20 @@ package aquaflow;
  */
 public enum Direction {
     Left, Right, Up, Down;
+
+    static Direction random() {
+        int num = (int) (Math.random() * (4 - 1 + 1) + 1);
+        switch (num) {
+            case 1:
+                return Left;
+            case 2:
+                return Right;
+            case 3:
+                return Up;
+            case 4:
+                return Down;
+            default:
+                return Right;
+        }
+    }
 }
